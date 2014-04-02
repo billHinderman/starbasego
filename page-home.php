d9675531d7104aa8f953af7ebdfb06a87a58194d
Bill Hinderman <bill.hinderman@orbitz.com>
2014-04-02 09:58:55 -0500

    more homepage changes

--- page-home.php
+++ page-home.php
@@ -24,10 +24,19 @@
 																'theme_location' => 'main-menu' ) ); ?>
 		</nav>
 		<div class="content" role="main">
-			<div class="welcome" data-bg-image="<?php bloginfo(template_url); ?>/assets/images/07.jpg" data-fill>
+			<div class="welcome" data-bg-image="<?php bloginfo(template_url); ?>/assets/images/06.jpg" data-fill>
 				<h1 class="welcome-title">starbase<br><b>go!</b></h1>
 			</div>
+
 			<div class="arrow"></div>
+
+<svg width="100%" height="500" xmlns="http://www.w3.org/2000/svg" >
+    <marker id="triangle" viewBox="0 0 10 10" refX="1" refY="5" markerUnits="strokeWidth" markerWidth="4" markerHeight="3" orient="auto">
+      <path d="M 0 0 L 10 5 L 0 10 z"/>
+    </marker>
+	<path d="M200,300 Q400,50 600,300 T1000,300" marker-end="url(#triangle)" stroke="black" stroke-width="10" fill="none"/>
+	<path d="M100,400 Q500,30 600,300 T800,300" marker-end="url(#triangle)" stroke="black" stroke-width="10" fill="none"/>
+</svg>
 
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
