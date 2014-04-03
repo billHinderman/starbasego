ded870d69af6025450471da703025d8a88194205
Bill Hinderman <bill.hinderman@orbitz.com>
2014-04-03 16:46:07 -0500

    more styles

--- page-home.php
+++ page-home.php
@@ -25,44 +25,37 @@
 		</nav>
 		<div class="content" role="main">
 			<section class="welcome" data-bg-image="<?php bloginfo(template_url); ?>/assets/images/06.jpg" data-fill>
-				<h1 class="welcome-title">starbase<br><b>go!</b></h1>
+				<h1 class="welcome-title">starbase<b>go!</b></h1>
 			</section>
 
 			<div class="arrow"></div>
 
-			<svg width="100%" height="500" xmlns="http://www.w3.org/2000/svg" >
-			    <marker id="triangle" viewBox="0 0 10 10" refX="1" refY="5" markerUnits="strokeWidth" markerWidth="4" markerHeight="3" orient="auto">
-			      <path d="M 0 0 L 10 5 L 0 10 z"/>
-			    </marker>
-				<path d="M200,300 Q400,50 600,300 T1000,300" marker-end="url(#triangle)" stroke="black" stroke-width="10" fill="none"/>
-				<path d="M100,400 Q500,30 600,300 T800,300" marker-end="url(#triangle)" stroke="black" stroke-width="10" fill="none"/>
-			</svg>
-
-
-			<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" xml:space="preserve">
-				<path d="M938.7,0L938.7 512 938.7 1024 853.3 1024 853.3 512 853.3 0 768 0 768 512 768 1024 682.7 1024 682.7 512 682.7 0 597.3 0 597.3 512 597.3 1024 426.7 1024 426.7 512 426.7 0 341.3 0 341.3 512 341.3 1024 256 1024 256 512 256 0 170.7 0 170.7 512 170.7 1024 85.3 1024 85.3 512 85.3 0 0 85.3 512 85.3 1024 85.3 1024 170.7 512 170.7 0 170.7 0 256 512 256 1024 256 1024 341.3 512 341.3 0 341.3 0 426.7 512 426.7 1024 426.7 1024 597.3 512 597.3 0 597.3 0 682.7 512 682.7 1024 682.7 1024 768 512 768 0 768 0 853.3 512 853.3 1024 853.3 1024 938.7 512 938.7 0 938.7 z" stroke="black" stroke-width="10" fill="none"/>
-			</svg>
-
 			<section class="about" data-fill>
-				blah
+				<div class="effect">
+				</div>
 			</section>
 
 			<section class="featured-projects" data-fill>
-				blah
+				<div class="effect">
+				</div>
 			</section>
 
 			<section class="featured-client" data-fill>
-				blah
+				<div class="effect">
+				</div>
 			</section>
 
 			<section class="team" data-fill>
-				blah
+				<div class="effect">
+				</div>
 			</section>
 
 			<section class="contact" data-fill>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<?php the_content(); ?>
-				<?php endwhile; endif; ?>
+				<div class="effect">
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+						<?php the_content(); ?>
+					<?php endwhile; endif; ?>
+				</div>
 			</section>
-			
+
 <?php get_footer(); ?>
