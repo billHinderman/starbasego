a217e74d5fd345a5b40b249100c807298d901a4e
Bill Hinderman <bill.hinderman@orbitz.com>
2014-05-05 16:55:59 -0500

    changes 5/5

--- entry.php
+++ entry.php
@@ -1,7 +1,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-	<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?> <?php edit_post_link(); ?>
-	<div class="chapter">
+	<?php if ( get_post_thumbnail_id() ) : ?>
+		<div class="chapter" data-fill data-bg-image="<?php $thumb_id = get_post_thumbnail_id();$thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true); echo $thumb_url[0]; ?>">
+	<?php else : ?>
+		<div class="chapter" data-fill  data-bg-image="<?php bloginfo(template_url); ?>/assets/images/colony/<?php echo str_pad(rand(1, 16), 2, "0", STR_PAD_LEFT); ?>.jpg">
+	<?php endif; ?>
+		<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?> <?php edit_post_link(); ?>
 		N°<?php the_ID(); ?>,
 		 <?php the_date( 'm/Y' ); ?> 
 	</div>
--- page-home.php
+++ page-home.php
@@ -24,7 +24,7 @@
 																'theme_location' => 'main-menu' ) ); ?>
 		</nav>
 		<div class="content" role="main">
-			<section class="welcome" data-bg-image="<?php bloginfo(template_url); ?>/assets/images/colony/02.jpg" data-fill>
+			<section class="welcome" data-bg-image="<?php bloginfo(template_url); ?>/assets/images/colony/<?php echo str_pad(rand(1, 16), 2, "0", STR_PAD_LEFT); ?>.jpg" data-fill>
 				<div class="effect">
 					<h1 class="welcome-title">starbase<br><b>go!</b></h1>
 				</div>
@@ -44,10 +44,27 @@
 			<section class="section featured-projects">
 				<div class="effect">
 					<h2>Our Projects</h2>
-					<?php query_posts('category_name=makes&tag=featured&posts_per_page=4'); ?>
-					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					   <?php the_content(); ?>
-					<?php endwhile; endif; ?>
+					<?php query_posts('category_name=makes&tag=featured&posts_per_page=2'); ?>
+					<?php if ( have_posts() ) : ?>
+						<ul class="float-clear">
+							<?php while ( have_posts() ) : the_post(); ?>
+								<?php if ( get_post_thumbnail_id() ) : ?>
+					   			<li class="project-post float-left" data-bg-image="<?php $thumb_id = get_post_thumbnail_id();$thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true); echo $thumb_url[0]; ?>">
+					   		<?php else : ?>
+									<li class="project-post float-left" data-bg-image="<?php bloginfo(template_url); ?>/assets/images/colony/<?php echo str_pad(rand(1, 16), 2, "0", STR_PAD_LEFT); ?>.jpg">
+								<?php endif; ?>
+					   			<div class="effect">
+								  <div class="post-chapter">
+										N°<?php the_ID(); ?>,<?php the_date( 'm/Y' ); ?> 
+									</div>
+								  <h3 class="post-title"><?php the_title(); ?></h3>
+								  <div class="post-excerpt"><?php the_excerpt(); ?></div>
+								  <a class="button" href="<?php echo get_permalink(); ?>"><i class="fa fa-rocket"></i> Read the dossier</a>
+									</div>
+					   		</li>
+							<?php endwhile;?>
+						</ul>
+					<?php endif; ?>
 					<?php wp_reset_query(); ?>
 				</div>
 			</section>
@@ -56,9 +73,26 @@
 				<div class="effect">
 					<h2>Our Clients</h2>
 					<?php query_posts('category_name=loves&tag=featured&posts_per_page=4'); ?>
-					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					   <?php the_content(); ?>
-					<?php endwhile; endif; ?>
+					<?php if ( have_posts() ) : ?>
+						<ul class="float-clear">
+							<?php while ( have_posts() ) : the_post(); ?>
+								<?php if ( get_post_thumbnail_id() ) : ?>
+					   			<li class="client-post float-left" data-bg-image="<?php $thumb_id = get_post_thumbnail_id();$thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true); echo $thumb_url[0]; ?>">
+					   		<?php else : ?>
+									<li class="client-post float-left" data-bg-image="<?php bloginfo(template_url); ?>/assets/images/colony/<?php echo str_pad(rand(1, 16), 2, "0", STR_PAD_LEFT); ?>.jpg">
+								<?php endif; ?>
+					   			<div class="effect">
+								  <div class="post-chapter">
+										N°<?php the_ID(); ?>,<?php the_date( 'm/Y' ); ?> 
+									</div>
+								  <h3 class="post-title"><?php the_title(); ?></h3>
+								  <div class="post-excerpt"><?php the_excerpt(); ?></div>
+								  <a class="button" href="<?php echo get_permalink(); ?>"><i class="fa fa-rocket"></i> Read the dossier</a>
+									</div>
+					   		</li>
+							<?php endwhile;?>
+						</ul>
+					<?php endif; ?>
 					<?php wp_reset_query(); ?>
 				</div>
 			</section>
@@ -80,19 +114,24 @@
 				</div>
 			</section>
 
-			<section class="section featured-blog">
+			<section class="section featured-blogs">
 				<div class="effect">
 					<h2>Our Thoughts</h2>
 					<?php query_posts('category_name=thinks&tag=featured&posts_per_page=2'); ?>
 					<?php if ( have_posts() ) : ?>
 						<ul class="float-clear">
 							<?php while ( have_posts() ) : the_post(); ?>
-								<li class="blog-post float-left"  data-bg-image="<?php $thumb_id = get_post_thumbnail_id();$thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true); echo $thumb_url[0]; ?>">
+								<?php if ( get_post_thumbnail_id() ) : ?>
+					   			<li class="blog-post float-left"  data-bg-image="<?php $thumb_id = get_post_thumbnail_id();$thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true); echo $thumb_url[0]; ?>">
+					   		<?php else : ?>
+									<li class="blog-post float-left" data-bg-image="<?php bloginfo(template_url); ?>/assets/images/colony/<?php echo str_pad(rand(1, 16), 2, "0", STR_PAD_LEFT); ?>.jpg">
+								<?php endif; ?>
 									<div class="effect">
 								  <div class="post-chapter">
 										N°<?php the_ID(); ?>,<?php the_date( 'm/Y' ); ?> 
 									</div>
 								  <h3 class="post-title"><?php the_title(); ?></h3>
+								  
 								  <div class="post-excerpt"><?php the_excerpt(); ?></div>
 								  <a class="button" href="<?php echo get_permalink(); ?>"><i class="fa fa-lightbulb-o"></i> Read our minds</a>
 									</div>
